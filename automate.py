567e1d248e9ea7a9baf62981d34ab7b0403274f8
sravan <sravan>
2025-01-01 15:39:35 -0500

    iterate

--- automate.py
+++ automate.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, StaleElementReferenceException
 import time  
 import os
 from dotenv import load_dotenv
@@ -67,10 +69,20 @@ try:
     time.sleep(3)
     submit = driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div[3]/div/button[2]/span')
     submit.click()
+    time.sleep(3)
+    clickable_elements = driver.find_elements(By.CLASS_NAME, "job-card-container__link")
 
+    for element in clickable_elements:
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable(element))
+            element.click()
+            time.sleep(2)  
+            print(f"Clicked on: {element.get_attribute('aria-label')}")  
+            time.sleep(2000)
+        except Exception as e:
+            print(f"Failed to click on an element: {e}")
 
-    time.sleep(3000)
-
-finally:
-    # Close the browser
+    driver.quit()
+except Exception as e:
+    print(f"An error occurred: {e}")
     driver.quit()
